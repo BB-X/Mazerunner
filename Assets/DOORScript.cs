5120f1face9798dd4cbe5f524701fbdb830cf72d
wonder_staff <wonder_staff@MAC20170615-6.local>
2019-04-13 11:33:35 +0900

    try commit

---
+++ Assets/DOORScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DOORScript : MonoBehaviour {
+
+    private Vector3 initialPosition;
+	// Use this for initialization
+	void Start () {
+        initialPosition = transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // transform.position = new Vector3 (Mathf.Sin(Time.time) * 2.0f + initialPosition.x, initialPosition.y, initialPosition.z);
+        Debug.Log(transform.position.x);
+        if (transform.position.x < -132.0f ){
+            transform.position = new Vector3(transform.position.x+0.01f, initialPosition.y, initialPosition.z);
+        }
+    }
+}
